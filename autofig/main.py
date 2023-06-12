c7138a0fa5b59938df6ba5d815675eb5f62b7926
comar <saywooong@gmail.com>
2023-06-12 18:50:28 +0900

    feat: gen config

--- autofig/main.py
+++ autofig/main.py
@@ -3,6 +3,10 @@ import argparse
 from mpl_toolkits.axes_grid1 import ImageGrid
 from omegaconf import OmegaConf
 from PIL import Image
+from os.path import join, dirname, basename
+from glob import glob
+import os
+import shutil
 
 
 def load_config(config_path):
@@ -10,9 +14,19 @@ def load_config(config_path):
     return config
 
 
+def gen_configs(arg):
+    path = join(dirname(__file__), "configs")
+    for path_f in glob(join(path, "*.yaml")):
+        file = basename(path_f)
+        if not os.path.exists(file):
+            shutil.copy(path_f, file)
+            print(f"Copy from {path_f} to {file}")
+
+
 def parse():
     p = argparse.ArgumentParser()
-    p.add_argument('--config', default='configs/config2x4_label.yaml')
+    p.add_argument('--config', default='autofig.yaml')
+    p.add_argument('-g', '--gen_config', action='store_true')
     p.add_argument('--output', default=None)
     return p.parse_args()
 
@@ -51,7 +65,9 @@ def plot_w_allcaption(images, labels, config):
             labelleft=False,
             labelbottom=False,
         )
-        ax.set_xlabel(label, fontsize=config.font.size, fontname=config.font.type)
+        ax.set_xlabel(label,
+                      fontsize=config.font.size,
+                      fontname=config.font.type)
 
     plt.tight_layout(pad=config.layout.margin, rect=(0, 0, 1, 1))
 
@@ -93,12 +109,18 @@ def plot_images(images, labels, ylabels, config):
         )
 
         if label is not None:
-            ax.set_xlabel(label, fontsize=config.font.size, fontname=config.font.type)
+            ax.set_xlabel(label,
+                          fontsize=config.font.size,
+                          fontname=config.font.type)
         if ylabel is not None:
-            ax.set_ylabel(ylabel, fontsize=config.font.size, fontname=config.font.type)
+            ax.set_ylabel(ylabel,
+                          fontsize=config.font.size,
+                          fontname=config.font.type)
+
 
 def extract_name(path):
     return path.split('/')[-1].split('.')[0]
+
 
 def main():
     print('Start autofig')
@@ -114,7 +136,7 @@ def main():
     else:
         plot_images(images, labels, ylabels, config)
 
-    path_output = f"{extract_name(args.config)}.{config.format}"  if args.output is None else args.output
+    path_output = f"{extract_name(args.config)}.{config.format}" if args.output is None else args.output
     plt.savefig(path_output, dpi=150, bbox_inches="tight")
 
 
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ Automatic figure generator such as image grids.
 python main.py --config configs/config1x4.yaml 
 ```
 <figure>
-<img src="assets/config1x4.jpg" alt="fail" style="width:100%">
+<img src="autofig/assets/config1x4.jpg" alt="fail" style="width:100%">
 </figure>
 
 
@@ -18,7 +18,7 @@ python main.py --config configs/config1x4.yaml
 python main.py --config configs/config2x4.yaml 
 ```
 <figure>
-<img src="assets/config2x4.jpg" alt="fail" style="width:100%">
+<img src="autofig/assets/config2x4.jpg" alt="fail" style="width:100%">
 </figure>
 
 #### Multiple rows example with single row label
@@ -27,7 +27,7 @@ python main.py --config configs/config2x4_single.yaml
 ```
 
 <figure>
-<img src="assets/config2x4_single.jpg" alt="fail" style="width:100%">
+<img src="autofig/assets/config2x4_single.jpg" alt="fail" style="width:100%">
 </figure>
 
 #### Multiple rows example with single row label and ylabel
@@ -36,5 +36,5 @@ python main.py --config configs/config2x4_ylab.yaml
 ```
 
 <figure>
-<img src="assets/config2x4_ylab.jpg" alt="fail" style="width:100%">
+<img src="autofig/assets/config2x4_ylab.jpg" alt="fail" style="width:100%">
 </figure>
