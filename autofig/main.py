5125575a654917d026e17d80f9d4d0088339939c
comar <saywooong@gmail.com>
2023-06-22 20:55:12 +0900

    fix: fail

--- autofig/main.py
+++ autofig/main.py
@@ -16,6 +16,31 @@ def load_config(config_path):
     return config
 
 
+def gen_custom_config(
+    row,
+    col,
+):
+    config = OmegaConf.load(join(dirname(__file__), 'configs/autofig.yaml'))
+    config.layout.num_col = col
+    config.layout.num_row = row
+
+    total = col * row
+
+    config.images = ["${Black}" for j in range(row) for i in range(col)]
+    config.labels = [
+        "(" + chr(ord('a') + i) + ")" if j == row - 1 else ''
+        for j in range(row) for i in range(col)
+    ]
+    config.ylabels = [
+        "(" + chr(ord('A') + j) + ")" if i == 0 else '' for j in range(row)
+        for i in range(col)
+    ]
+
+    config_str = OmegaConf.to_yaml(config)
+    with open('autofig.yaml', 'w') as file:
+        file.write(config_str)
+
+
 def parse():
     p = argparse.ArgumentParser()
     p.add_argument('--config', default='autofig.yaml')
